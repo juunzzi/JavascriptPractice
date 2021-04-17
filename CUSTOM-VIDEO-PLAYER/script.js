338db33ca5fb25fb9d6a9fedc83e21c8e441fbee
junzzi <jjh01027717502@gmail.com>
2021-04-18 01:39:47 +0900

    feat: update CUSTOM-VIDEO-PLAYER

--- CUSTOM-VIDEO-PLAYER/script.js
+++ CUSTOM-VIDEO-PLAYER/script.js
@@ -4,13 +4,6 @@ const controls = document.querySelector(".controls");
 const videoRange = document.querySelector("#progress");
 const timeStamp = document.querySelector("#timestamp");
 const video = document.querySelector("#video");
-const setTimeStamp = (videoCurrentTime) => {
-  const min = Number.parseInt(videoCurrentTime / 60);
-  const sec = Number.parseInt(videoCurrentTime % 60);
-  timeStamp.innerText = `${min < 10 ? `0${min}` : min}:${
-    sec < 10 ? `0${sec}` : sec
-  }`;
-};
 const videoStart = function () {
   this.className = "fa fa-pause fa-2x";
   videoRange.max = video.duration;
@@ -22,7 +15,13 @@ const videoPause = function () {
   playPauseButton.className = "fa fa-play fa-2x";
   video.pause();
 };
-
+const setTimeStamp = (videoCurrentTime) => {
+  const min = Number.parseInt(videoCurrentTime / 60);
+  const sec = Number.parseInt(videoCurrentTime % 60);
+  timeStamp.innerText = `${min < 10 ? `0${min}` : min}:${
+    sec < 10 ? `0${sec}` : sec
+  }`;
+};
 const videoTimeUpdateHandler = () => {
   videoRange.value = video.currentTime;
   setTimeStamp(video.currentTime);
@@ -45,11 +44,9 @@ const videoControlsHandler = function handler(e) {
 
 controls.addEventListener("click", videoControlsHandler);
 
-// controls.addEventListener.apply(playPauseButton, [
-//   "click",
-//   function handler() {
-//     console.log(this);
-//   },
-// ]);
-/// controls에서 재생버튼을 누를때만 HANDLER가 실행된다.. this로 보내준 요소를 클릭했을때만
-/// handler가 실행됨..
+// controls.addEventListener("click", function () {
+//   console.log(this);
+// });
+
+// / controls에서 재생버튼을 누를때만 HANDLER가 실행된다.. this로 보내준 요소를 클릭했을때만
+// / handler가 실행됨..
