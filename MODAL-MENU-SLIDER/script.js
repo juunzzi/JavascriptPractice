93c21059724378f6e23cad24930422821c09773c
junzzi <jjh01027717502@gmail.com>
2021-05-09 01:12:21 +0900

    feat: update MODAL-MENU-SLIDER

--- MODAL-MENU-SLIDER/script.js
+++ MODAL-MENU-SLIDER/script.js
@@ -1,13 +1,10 @@
-const modal = document.querySelector(".modal-container");
-const body = document.querySelector("body");
 const toggleBtn = document.querySelector("#toggle");
 const ctaBtn = document.querySelector(".cta-btn");
 const closeBtn = document.querySelector(".close-btn");
-const btnHandler = (element, className) => {
-  return () => {
-    element.classList.toggle(className);
-  };
+const btnHdr = (elementName, className) => {
+  const element = document.querySelector(elementName);
+  return () => element.classList.toggle(className);
 };
-toggleBtn.addEventListener("click", btnHandler(body, "show-nav"));
-ctaBtn.addEventListener("click", btnHandler(modal, "show-modal"));
-closeBtn.addEventListener("click", btnHandler(modal, "show-modal"));
+toggleBtn.addEventListener("click", btnHdr("body", "show-nav"));
+ctaBtn.addEventListener("click", btnHdr(".modal-container", "show-modal"));
+closeBtn.addEventListener("click", btnHdr(".modal-container", "show-modal"));
